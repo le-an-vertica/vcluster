650f7a57387c28024df3d033d923f1b5cc58ba2d
Le An <le.an@vertica.com>
2024-09-04 11:29:24 -0700

    Add k8s option for fetch_node_state.go

--- vclusterops/fetch_node_state.go
+++ vclusterops/fetch_node_state.go
@@ -72,8 +72,14 @@ func (vcc VClusterCommands) VFetchNodeState(options *VFetchNodeStateOptions) ([]
 
 	// this vdb is used to fetch node version
 	var vdb VCoordinationDatabase
-	err = vcc.getVDBFromRunningDBIncludeSandbox(&vdb, &options.DatabaseOptions, util.MainClusterSandbox)
-	if err != nil {
+
+        if util.IsK8sEnvironment() {
+		err = vcc.getVDBFromRunningDBIncludeSandbox(&vdb, &options.DatabaseOptions, util.MainClusterSandbox)
+                } else {
+		err = vcc.getVDBFromMainRunningDBContainsSandbox(&vdb, &options.DatabaseOptions)
+	}
+
+        if err != nil {
 		vcc.Log.PrintInfo("Error from vdb build: %s", err.Error())
 
 		rfcError := &rfc7807.VProblem{}
